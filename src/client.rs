6ca0a39e4b3fdb20fbb8b68c8737358b2033fd5a
Pi Songkuntham <pisong@princeton.edu>
2020-01-08 09:18:37 +0700

    update client to talk to eu and jp

--- src/client.rs
+++ src/client.rs
@@ -6,7 +6,7 @@ extern crate sharedlib;
 use grpc::ClientStub;
 use rayon::prelude::*;
 use sharedlib::epaxos_grpc::*;
-use sharedlib::logic::{WriteRequest, OR, REPLICA_PORT};
+use sharedlib::logic::{WriteRequest, EU, JP, REPLICA_PORT};
 use std::{sync::Arc, time::Instant};
 
 fn main() {
@@ -14,19 +14,28 @@ fn main() {
         key: "pi".to_string(),
         value: 1,
     };
-    let mut write_reqs = vec![write_req1.to_grpc(); 10];
-    write_reqs.par_iter_mut().enumerate().for_each(|(i, req)| {
-        let grpc_client =
-            Arc::new(grpc::Client::new_plain(OR, REPLICA_PORT, Default::default()).unwrap());
-        let client = EpaxosServiceClient::with_client(grpc_client);
-        let start = Instant::now();
-        let res = client.write(grpc::RequestOptions::new(), req.clone());
-        match res.wait() {
-            Err(e) => panic!("Write Failed: {}", e),
-            Ok((_, _, _)) => {
-                let duration = start.elapsed();
-                println!("{} Commit Latency: {:?}", i, duration);
+    let write_req2 = WriteRequest {
+        key: "pi".to_string(),
+        value: 2,
+    };
+    let mut write_reqs = Vec::new();
+    write_reqs.push((write_req1.to_grpc(), JP));
+    write_reqs.push((write_req2.to_grpc(), EU));
+    write_reqs
+        .par_iter_mut()
+        .enumerate()
+        .for_each(|(i, (req, dst))| {
+            let grpc_client =
+                Arc::new(grpc::Client::new_plain(dst, REPLICA_PORT, Default::default()).unwrap());
+            let client = EpaxosServiceClient::with_client(grpc_client);
+            let start = Instant::now();
+            let res = client.write(grpc::RequestOptions::new(), req.clone());
+            match res.wait() {
+                Err(e) => panic!("Write Failed: {}", e),
+                Ok((_, _, _)) => {
+                    let duration = start.elapsed();
+                    println!("{} Commit Latency: {:?}", i, duration);
+                }
             }
-        }
-    });
+        });
 }
