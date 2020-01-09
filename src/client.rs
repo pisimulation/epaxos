740f8e4c9c331fc037853ead7fd10d2ede959e71
Pi Songkuntham <pisong@princeton.edu>
2020-01-09 09:40:31 +0700

    edited req

--- src/client.rs
+++ src/client.rs
@@ -6,27 +6,30 @@ extern crate sharedlib;
 use grpc::ClientStub;
 use rayon::prelude::*;
 use sharedlib::epaxos_grpc::*;
-use sharedlib::logic::{WriteRequest, EU, JP, REPLICA_PORT};
-use std::{sync::Arc, time::Instant};
+use sharedlib::logic::{WriteRequest, REPLICA_ADDRESSES, REPLICA_PORT};
+use std::{env, sync::Arc, time::Instant};
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let id: u32 = args[1].parse().unwrap();
     let write_req1 = WriteRequest {
         key: "pi".to_string(),
         value: 1,
     };
-    let write_req2 = WriteRequest {
-        key: "pi".to_string(),
-        value: 2,
-    };
     let mut write_reqs = Vec::new();
-    write_reqs.push((write_req1.to_grpc(), JP));
-    write_reqs.push((write_req2.to_grpc(), EU));
+    write_reqs.push((write_req1.to_grpc(), id));
     write_reqs
         .par_iter_mut()
         .enumerate()
-        .for_each(|(i, (req, dst))| {
-            let grpc_client =
-                Arc::new(grpc::Client::new_plain(dst, REPLICA_PORT, Default::default()).unwrap());
+        .for_each(|(i, (req, id))| {
+            let grpc_client = Arc::new(
+                grpc::Client::new_plain(
+                    REPLICA_ADDRESSES[*id as usize],
+                    REPLICA_PORT,
+                    Default::default(),
+                )
+                .unwrap(),
+            );
             let client = EpaxosServiceClient::with_client(grpc_client);
             let start = Instant::now();
             let res = client.write(grpc::RequestOptions::new(), req.clone());
